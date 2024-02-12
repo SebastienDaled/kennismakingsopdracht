c623077730c8f5b68fcdec7408b5f19336ec9696
{SebastienDaled} <{sebadale@student.arteveldehs.be}>
2024-02-12 10:46:28 +0100

    WIP

--- web/modules/custom/ai_chatbot/src/Form/AiChatbotForm.php
+++ web/modules/custom/ai_chatbot/src/Form/AiChatbotForm.php
@@ -10,6 +10,7 @@ use Drupal\media\Entity\Media;
 use Drupal\node\Entity\Node;
 use Drupal\Core\File\FileSystemInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Validator\Constraints\All;
 
 class AiChatbotForm extends FormBase {
 
@@ -57,74 +58,66 @@ class AiChatbotForm extends FormBase {
    * x@xxxxxxxxxxx
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
-    $nodes_offices = \Drupal::entityTypeManager()->getStorage('node')->loadByProperties(['type' => 'offices']);
     $nodes_news = \Drupal::entityTypeManager()->getStorage('node')->loadByProperties(['type' => 'news']);
     $nodes_articles = \Drupal::entityTypeManager()->getStorage('node')->loadByProperties(['type' => 'article']);
 
-    $office_values = [];
-    foreach ($nodes_offices as $node) {
-      // dd($node->values['body']);
-      // $office_values[$node->id()] = $node->getTitle();
+    // make an object with all the bodys fromeach content type
+    $all_data = [];
+
+    // Counter for creating keys
+    $counter = 1;
+
+    foreach($nodes_news as $news) {
+        $body_value = $news->body->value;
+
+        $key = 'news' . $counter;
+
+        $all_data[$key] = $body_value;
+
+        $counter++;
+    }
+    $counter = 1;
+    foreach($nodes_articles as $article) {
+      $body_value = $article->body->value;
+
+      $key = 'article' . $counter;
+
+      $all_data[$key] = $body_value;
+
+      $counter++;
     }
 
-    $nodes = array_merge($nodes_offices, $nodes_news, $nodes_articles);
+    $json_all_data = json_encode($all_data);
+
+    $api_key = Settings::get('api_key');
+
+    $prompt_user = $form_state->getValue('prompt');
     
-    dd($nodes);
-
-    // $api_key = "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";
-
-    // $prompt_user = $form_state->getValue('prompt');
-
-    // // index all content from articles, news and offices
-    // $all_content_query = \Drupal::entityQuery('node')
-    //   ->accessCheck(FALSE)
-    //   ->condition('type', ['article', 'news', 'office'], 'IN')
-    //   ->execute();
-
-    // $all_content = Node::loadMultiple($all_content_query);
-    // $content_string = "";
-    // // get the body of each content into a string
-    // foreach ($all_content as $content) {
-    //   $body = $content->get('body')->value;
-    //   $content_string .= $body;
-    // }
-    // // dd($content_string);
+    $prompt_ai = "there is a json object with data, you need to you to answer the question that is being asked by using the json object. the json object has 'news1', 'news2,... and 'article1', 'article2',... each one has a text that contains a info about the article or news. the question being asked has to be anwered with the data in the json format. json data: " . $json_all_data . ". question: " . $prompt_user . ".";
     
-    // $prompt_ai = "this is al the content that needs to be searched: " . $content_string . ". the following prompt asks for more info about something from the content" . $prompt_user;
-    // // $prompt_ai = $prompt_user . ": " . $content_string;
-
-    // $response = get_ai_response($prompt_ai, $api_key);
+    $response = get_ai_response($prompt_ai, $api_key);
     
-
-    // $form['result'] = [
-    //   '#type' => 'item',
-    //   '#markup' => $response,
-    //   '#attributes' => [
-    //     'class' => ['form-result']
-    //   ]
-    // ];
-
+    dd($response);
   }
 }
 
-// function get_ai_response($prompt, $api_key) {
-//   $url = 'https://api.openai.com/v1/completions';
-//   $client = new Client();
-//   $model = "gpt-3.5-turbo-instruct";
+function get_ai_response($prompt, $api_key) {
+  $url = 'https://api.openai.com/v1/completions';
+  $client = new Client();
+  $model = "gpt-3.5-turbo-instruct";
 
-//   $response = $client->post($url, [
-//     'headers' => [
-//       'Authorization' => 'Bearer ' . $api_key,
-//       'Content-Type' => 'application/json',
-//     ],
-//     'json' => [
-//       'prompt' => $prompt,
-//       'temperature' => 0.7,
-//       'max_tokens' => 150,
-//       'model' => $model,
-//     ],
-//   ]);
-//   $response = json_decode($response->getBody()->getContents())->choices[0]->text;
-//   dd($response);
-//   return $response;
-// }
+  $response = $client->post($url, [
+    'headers' => [
+      'Authorization' => 'Bearer ' . $api_key,
+      'Content-Type' => 'application/json',
+    ],
+    'json' => [
+      'prompt' => $prompt,
+      'temperature' => 0.7,
+      'max_tokens' => 150,
+      'model' => $model,
+    ],
+  ]);
+  $response = json_decode($response->getBody()->getContents())->choices[0]->text;
+  return $response;
+}
--- web/modules/custom/ai_chatbot/src/Plugin/Block/AiChatbotBlock.php
+++ web/modules/custom/ai_chatbot/src/Plugin/Block/AiChatbotBlock.php
@@ -20,6 +20,7 @@ class AiChatbotBlock extends BlockBase {
   public function build() {
     $form = \Drupal::formBuilder()->getForm('Drupal\ai_chatbot\Form\AiChatbotForm');
     return $form;
+
   }
 
 }
